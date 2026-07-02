8d466f444645ee11166cae379cc6bc405b990b89
mleden13 <mlspam11@gmail.com>
2026-07-02 07:43:37 +0200

    Update refresh-token-puppeteer.js

--- scripts/refresh-token-puppeteer.js
+++ scripts/refresh-token-puppeteer.js
@@ -95,6 +95,10 @@ async function getRefreshToken() {
     });
     
     const page = await browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36');
+    await page.setExtraHTTPHeaders({
+      'Accept-Language': 'pl-PL,pl;q=0.9,en;q=0.8'
+    });
     await page.setViewport({ width: 1280, height: 720 });
     
     // Monitoruj wszystkie responses
