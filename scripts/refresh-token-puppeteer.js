606529fcfaca969b3235f96be72f6d273a11c6c8
mleden13 <mlspam11@gmail.com>
2026-07-02 07:42:18 +0200

    Update refresh-token-puppeteer.js

--- scripts/refresh-token-puppeteer.js
+++ scripts/refresh-token-puppeteer.js
@@ -1,4 +1,6 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
 const axios = require('axios');
 
 const ENERGA_EMAIL = process.env.ENERGA_EMAIL;
